5d09ec1ccd15f507e5298c41c2d53bb78d091180
FiremasterGR <firemastergr1@protonmail.com>
2026-05-03 14:10:43 +0000

    Update signup.php

--- public/pages/signup.php
+++ public/pages/signup.php
@@ -13,7 +13,6 @@ include ('nonce.php');
   <script src="js/visibilitychange.js" async defer></script>
   <script src="js/inputCheck.js" async defer></script>
   <script src="js/LocalDatabase.js" async defer></script>
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/style4.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
