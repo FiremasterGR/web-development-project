2097aaac45dea45f7f13249993470a272ab0b89c
FiremasterGR <firemastergr1@protonmail.com>
2026-05-03 14:06:03 +0000

    Update contact.php

--- public/pages/contact.php
+++ public/pages/contact.php
@@ -7,13 +7,12 @@
 <head>
   <title>Επικοινωνία</title>
   <meta charset="UTF-8">
-  <script src="loadingscreen.js"></script>
-  <script src="dragstopper.js" async defer></script>
-  <script src="inputCheck.js" async defer></script>
-  <script src="LocalDatabase.js" async defer></script>
+  <script src="js/loadingscreen.js"></script>
+  <script src="js/inputCheck.js" async defer></script>
+  <script src="js/LocalDatabase.js" async defer></script>
   <script nonce="<?php echo $nonce; ?>" src="https://www.google.com/recaptcha/api.js?hl=el&onload=initCaptchaScaling" async defer></script>
-  <script src="captcha_scaling.js"></script>
-  <link rel="stylesheet" href="style.css">
+  <script src="js/captcha_scaling.js"></script>
+  <link rel="stylesheet" href="css/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
