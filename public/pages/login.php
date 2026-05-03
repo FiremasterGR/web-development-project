e17b68d6c3643a7edba5605ee734f70294b8a37a
FiremasterGR <firemastergr1@protonmail.com>
2026-05-03 14:10:53 +0000

    Update login.php

--- public/pages/login.php
+++ public/pages/login.php
@@ -7,7 +7,6 @@
 <head>
   <title>Σύνδεση</title>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="css/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <script src="js/visibilitychange.js" async defer></script>
   <script src="js/inputCheck.js" async defer></script>
