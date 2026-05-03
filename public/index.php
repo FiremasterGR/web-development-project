6a7cd66f4d6186d9a105ed3e077cb9497a5fc031
FiremasterGR <firemastergr1@protonmail.com>
2026-05-03 14:10:34 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,9 +8,6 @@
     <title>Αρχική</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <script src="loadingscreen.js"></script>
-    <script src="dragstopper.js" async defer></script>
-    <link rel="stylesheet" href="style.css">
     <style>
         .speciallink{
             display: block;
