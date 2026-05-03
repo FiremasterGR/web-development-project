c06007089e5f47a08e2e9bdd3156e05494631a69
FiremasterGR <firemastergr1@protonmail.com>
2026-05-03 14:11:02 +0000

    Update house_selling2.php

--- public/pages/house_selling2.php
+++ public/pages/house_selling2.php
@@ -12,7 +12,6 @@ include ('nonce.php');
   <script src="js/lightbox.js" async defer></script>
   <script src="js/LocalDatabase.js" async defer></script>
   <script src="js/numrestrict.js" async defer></script>
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/style2.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
